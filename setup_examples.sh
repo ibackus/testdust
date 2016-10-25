c0ee936ba5135ba6f9f047bb1be14c4da50758b7
Isaac Backus <ibackus@gmail.com>
2016-10-25 16:41:12 -0700

    Added setup_examples.sh script

---
+++ setup_examples.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Check for overwriting
+if [ -d "examples/" ]; then
+    while true; do
+        read -p "Overwrite your copies of examples [y/n]? " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) echo "skipping"; exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
+# Copy examples
+rsync -avI src/examples/ examples/
+rm examples/README.md
+echo "Success.  See examples in examples/ folder"
+
---
+++ src/README.md
@@ -0,0 +1,3 @@
+Do not edit/use the files in this directory.  Run setup_examples.sh in the
+repo base directory and use the generated folder
+
